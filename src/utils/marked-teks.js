8c979bc8bf4a10b2976c910a73efc97ea1e3add5
Asygarr <105841104721@student.unismuh.ac.id>
2025-06-23 22:58:15 +0800

    buat mark response Ai

--- src/utils/marked-teks.js
+++ src/utils/marked-teks.js
@@ -1,5 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { marked } from "marked";
 
-export function formatMarkdownResponse(response) {
-  return { __html: marked(response) };
+export default function ResultBox({ resultText }) {
+  const [htmlContent, setHtmlContent] = useState("");
+
+  useEffect(() => {
+    if (resultText) {
+      const html = marked.parse(resultText);
+      setHtmlContent(html);
+    }
+  }, [resultText]);
+
+  return (
+    <div className="mt-4 space-y-4">
+      <div className="p-4 bg-gray-100 border rounded text-gray-700 prose max-w-none">
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      </div>
+    </div>
+  );
 }
